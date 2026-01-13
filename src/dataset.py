ec902f108e527028b516076656b91aac52a046e5
Pat <Pat>
2026-01-13 10:55:04 -0700

    first commit

---
+++ src/dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [30, 25, 35],
+    "City": ["New York", "Los Angeles", "Chicago"],
+}
+df = pd.DataFrame(data)
+
+print(df)
